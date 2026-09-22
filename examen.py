c0faaec7edbcd6ddfcb2e25927dc686e0eb60952
c11-alonso <calonso@escuelalosandes.edu.ar>
2026-09-22 11:46:45 -0300

    primera etapa

--- examen.py
+++ examen.py
@@ -1,6 +1,6 @@
 # Examen práctico - Terminal de Expedición Espacial
-# Nombre y apellido:
-# Curso:
+# Nombre y apellido: Catalina Alonso
+# Curso:2° 1°
 #
 # IMPORTANTE:
 # Resolver el programa siguiendo las etapas indicadas en el README.md.
@@ -11,6 +11,20 @@
 
 # =========================
 # ETAPA 1 - INICIO
+nombrep = input("ingresar el nombre del piloto")
+print ("bienvenido/a", nombrep, "el combustible disponible inicial es 100")
+cantidadv = 0
+print ("cantidad de viajes realizados es cero", cantidadv)
+cantidadvl = 0
+print ("cantidad de viajes realizados a la lunaes cero", cantidadvl)
+cantidadvm = 0
+print ("cantidad de viajes realizados a marte es cero", cantidadvm)
+cantidadvs = 0
+print ("cantidad de viajes realizados a saturno es cero", cantidadvs)
+destinos = ["luna", "marte", "saturno"]
+costos = [20, 35, 50]
+
+
 # =========================
 
 # Crear las variables necesarias.
